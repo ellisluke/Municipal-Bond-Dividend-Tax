3ab92aece55084781646a2809f0708f24c1f748e
ellisluke <lke8@cornell.edu>
2025-03-06 17:51:23 -0500

    feat: set static folder

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
